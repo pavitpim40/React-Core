fd3746b7540d1a8cd6760486517726b7b9a045ba
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-21 16:26:56 +0700

    state

--- 06-state/_demo/main.js
+++ 06-state/_demo/main.js
@@ -7,21 +7,35 @@ function App() {
     // let updateFunction = myState[1]; // b
 
     const [count, setCount] = React.useState(0);
+    const [toggle, setToggle] = React.useState(true);
+    const [username, setUsername] = React.useState('');
 
-    // let count = 0;
+    let num = 0;
     const handleClick = () => {
         // count += 1;
         // console.log(count);
         let newState = count + 1;
+        // count += 1;
         setCount(newState);
+        setToggle(!toggle);
+        // num += 10;
+        // console.log(num);
     };
+
+    const handleChange = (e) => {
+        // console.log(e.target.value);
+        setUsername(e.target.value);
+    };
+
+    // console.log(username);
 
     // #UI
     return (
         <div className='container'>
-            State
+            State : {toggle && 'Hello'}
             <button onClick={handleClick}>Click me</button>
             <p>{count}</p>
+            <input onChange={(e) => setUsername(e.target.value)} value={username} />
         </div>
     );
 }
